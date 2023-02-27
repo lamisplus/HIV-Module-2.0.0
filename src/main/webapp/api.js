987c28e8421709884eddeb1eae8d43c36368cf9c
Mathew77 <mathewadegbite@gmail.com>
2023-02-27 16:57:27 +0100

    fixing the pharmcy refill

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -1,4 +1,4 @@
 export const  token = (new URLSearchParams(window.location.search)).get("jwt")
 export const url = '/api/v1/'
 // export const url =  'http://localhost:8383/api/v1/';
-// export const  token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjc3MjQ2MTI1fQ.l5vHUJBot37bMWxaIHJkg9KfgO_flerjo_ZxHF0T5Wkd8QpB6rcnAwc9NAbDfKF7wr2sTxHmMjgHcA_9eqwlPw'
+// export const  token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjc3NTE0MzUwfQ.ydtvHAR1A4dj-2QZy61n5CmGjwf0hH_eIQW3x0XhnDE1FeDeHgudfD6PTGWz_wS3VMeVTpSVHqGP5eHMiUdt9g'
