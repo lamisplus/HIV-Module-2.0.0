110c454b5aa1f31131cffe8738f649a253728362
Mathew77 <mathewadegbite@gmail.com>
2023-04-06 20:05:25 +0100

    implementing IPT logic on pharmacy

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -2,4 +2,4 @@ export const  token = (new URLSearchParams(window.location.search)).get("jwt")
 export const url = '/api/v1/'
 
 // export const url =  'http://localhost:8383/api/v1/';
-// export const  token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjgwNzk5MDA5fQ.YVaicfmLrQ3c-f1oJdAjjMnnsS__6cJnoZ2-Bg2XCTV0x4e3hgFb8_9W8zfs5Sa8MNYBqCI0Xc7TjLEq2SGj7g'
+// export const  token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjgwODMxMDUyfQ.dnWUONKjrD74sIBKHPH_83U1tIQ7vOEJL0awAR0UrotEridoIHHO7qZNTTGRRnAMCkZCn07vMNbdw9FAilovUg'
--- src/main/webapp/jsx/components/Pharmacy/PharmacyRefillNew.js
+++ src/main/webapp/jsx/components/Pharmacy/PharmacyRefillNew.js
@@ -945,7 +945,7 @@ const Pharmacy = (props) => {
         return prev + +duration
       }, 0);
 
-      
+  
 
   return (      
       <div>
@@ -1595,7 +1595,8 @@ const Pharmacy = (props) => {
                 
                 </FormGroup>
             </div>
-            {(showIptType && iptEligibilty.IPTEligibility===true ) && ( //iptEligibilty
+            
+            { iptEligibilty.IPTEligibility===true  && ( //iptEligibilty
             <div className="form-group mb-3 col-xs-6 col-sm-6 col-md-6 col-lg-6">
                 <FormGroup>
                 <Label >Visit Type</Label>
