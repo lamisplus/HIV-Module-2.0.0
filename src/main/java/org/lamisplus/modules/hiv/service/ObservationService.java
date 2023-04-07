91aaf98d1056ee3a3f0fef7fadb2790abe7e766b
aniwange33 <terteseamos@gmail.com>
2023-04-07 07:58:43 +0100

    feat(IPTEligibility): always update IPT details from pharmacy

--- src/main/java/org/lamisplus/modules/hiv/service/ObservationService.java
+++ src/main/java/org/lamisplus/modules/hiv/service/ObservationService.java
@@ -1,11 +1,15 @@
 package org.lamisplus.modules.hiv.service;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.lamisplus.modules.base.controller.apierror.EntityNotFoundException;
 import org.lamisplus.modules.base.controller.apierror.RecordExistException;
 import org.lamisplus.modules.hiv.domain.dto.ObservationDto;
+import org.lamisplus.modules.hiv.domain.entity.ArtPharmacy;
 import org.lamisplus.modules.hiv.domain.entity.Observation;
+import org.lamisplus.modules.hiv.repositories.ArtPharmacyRepository;
 import org.lamisplus.modules.hiv.repositories.ObservationRepository;
 import org.lamisplus.modules.patient.domain.entity.Person;
 import org.lamisplus.modules.patient.domain.entity.Visit;
@@ -27,6 +31,8 @@ public class ObservationService {
     private final CurrentUserOrganizationService currentUserOrganizationService;
     
    private final HandleHIVVisitEncounter handleHIVisitEncounter;
+   
+   private final ArtPharmacyRepository pharmacyRepository;
 
     public ObservationDto createAnObservation(ObservationDto observationDto) {
         Long personId = observationDto.getPersonId ();
@@ -38,6 +44,7 @@ public class ObservationService {
             if(observation.getType().equals("Clinical evaluation"))
                 throw new RecordExistException (Observation.class, "type", observationDto.getType ());
         }
+        processAndUpdateIptFromPharmacy(observationDto, person);
         observationDto.setFacilityId (orgId);
         Visit visit = handleHIVisitEncounter.processAndCreateVisit (personId, observationDto.getDateOfObservation());
         if (visit != null) {
@@ -54,7 +61,40 @@ public class ObservationService {
         observationDto.setId (saveObservation.getId ());
         return observationDto;
     }
-
+    
+   
+    private void processAndUpdateIptFromPharmacy(ObservationDto observationDto, Person person) {
+        if(observationDto.getType().equals("Chronic Care")){
+            JsonNode tptMonitoring = observationDto.getData().get("tptMonitoring");
+            JsonNode iptCompletionDate = tptMonitoring.get("date");
+            JsonNode outComeOfIpt = tptMonitoring.get("outComeOfIpt");
+            if( outComeOfIpt != null || iptCompletionDate != null){
+                StringBuilder dateIptCompleted = new StringBuilder();
+                StringBuilder iptCompletionStatus = new StringBuilder();
+                
+                if(iptCompletionDate != null ){
+                    dateIptCompleted.append(iptCompletionDate.asText());
+                }
+                if(outComeOfIpt != null){
+                    iptCompletionStatus.append(outComeOfIpt.asText());
+                }
+                Optional<ArtPharmacy> recentIPtPharmacy =
+                        pharmacyRepository.getPharmacyIpt(person.getUuid());
+                if(recentIPtPharmacy.isPresent()){
+                    ArtPharmacy artPharmacy = recentIPtPharmacy.get();
+                    JsonNode ipt = artPharmacy.getIpt();
+                    ((ObjectNode) ipt).put("dateCompleted", dateIptCompleted.toString());
+                    ((ObjectNode) ipt).put("completionStatus", iptCompletionStatus.toString());
+                    artPharmacy.setIpt(ipt);
+                    pharmacyRepository.save(artPharmacy);
+                }
+                
+            }
+            
+            System.out.println(""+ tptMonitoring);
+        }
+    }
+    
     private Optional<Observation> getAnExistingObservationType(ObservationDto observationDto, Person person, Long orgId) {
         return observationRepository
                 .getAllByTypeAndPersonAndFacilityIdAndArchived (observationDto.getType (), person, orgId, 0);
@@ -126,4 +166,5 @@ public class ObservationService {
           map.put("IPTEligibility", iptEligiblePatientUuid.isPresent());
         return map;
     }
+    
 }
