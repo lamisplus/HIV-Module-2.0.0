792d5797f8132fb13560166fd7e353138250b131
Mathew77 <mathewadegbite@gmail.com>
2023-07-12 13:23:37 +0100

    fixing phone number being null on patient dashboard

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -1,5 +1,5 @@
 export const  token = (new URLSearchParams(window.location.search)).get("jwt")
 export const url = '/api/v1/'
 
-// export const url =  'http://localhost:8789/api/v1/';
-// export const  token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjg4NTAxNjAyfQ.ZhsWrDxGjC9NIxoR474J9GysUn-BVREHroQEIBdPYi_hWojRusd8I_gfu_bAh726PlmzTrenDYWhAj0g-ORwZA';
+// export const url =  'http://lamisplus.org:7090/api/v1/';
+// export const  token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjg5MTg4MTIyfQ.oFk3O0651GpPcPK9BXRJ3NwZb8RPf_7UUttyuldtjF4ay4EkGBD6rEX5xm8zeLoYSSu2Si9KgRJN0noVlwKUzQ';
