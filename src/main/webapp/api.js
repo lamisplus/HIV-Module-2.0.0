b0ff5ac9f1f7a666db39dcd1e2045ce59f78db11
Mathew77 <mathewadegbite@gmail.com>
2023-08-01 15:32:50 +0100

    fixng chronic eligibility date field onchange

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -1,5 +1,5 @@
-export const  token = (new URLSearchParams(window.location.search)).get("jwt")
-export const url = '/api/v1/'
+// export const  token = (new URLSearchParams(window.location.search)).get("jwt")
+// export const url = '/api/v1/'
 
-// export const url =  'http://lamisplus.org:7090/api/v1/';
-// export const  token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjg5MTg4MTIyfQ.oFk3O0651GpPcPK9BXRJ3NwZb8RPf_7UUttyuldtjF4ay4EkGBD6rEX5xm8zeLoYSSu2Si9KgRJN0noVlwKUzQ';
+export const url =  'http://localhost:8789/api/v1/';
+export const  token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjkwODg3OTIwfQ.TaO4Qgo3v-Ql0jllcWgIPBylRAR8CStTxYPjq4Mft6Ul1QHK5uhLwS5rOxbaWI-q5QC0BJD0tro7x4SBRZtuJQ';
