e86be382f4c23bc9394868e315cf007aad197eda
Mathew77 <mathewadegbite@gmail.com>
2023-08-04 13:38:50 +0100

    commenting out OTZ in the submenu.js

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -2,4 +2,4 @@ export const  token = (new URLSearchParams(window.location.search)).get("jwt")
 export const url = '/api/v1/'
 
 // export const url =  'http://localhost:8789/api/v1/';
-// export const  token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjkxMDc4NTgwfQ.zjb9aoRlHWreoiJHL2I8Ugf2jQ1JwwPjdvz-QEZwgH4lNnksty7LySuZW-3rKFm7dE1aaUnXCOExFMosHyKhZA';
+// export const  token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjkxMTU3Nzc5fQ.AROLj7d3Jb1BQRIuzxToBDDkjtiIX6V5IfORuIdkQqgx575ZEr3pH7l0As1JhgayR5Rw1SItzu3jzTmrgBKoPw';
