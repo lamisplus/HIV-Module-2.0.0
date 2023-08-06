f4b15d18f69e9609855e3fa50080bcf9cce1220c
Mathew77 <mathewadegbite@gmail.com>
2023-08-06 10:28:36 +0100

    set pregnacy status in art commencement to null value by default

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -1,5 +1,5 @@
 export const  token = (new URLSearchParams(window.location.search)).get("jwt")
 export const url = '/api/v1/'
 
-// export const url =  'http://localhost:8789/api/v1/';
-// export const  token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjkxMTU3Nzc5fQ.AROLj7d3Jb1BQRIuzxToBDDkjtiIX6V5IfORuIdkQqgx575ZEr3pH7l0As1JhgayR5Rw1SItzu3jzTmrgBKoPw';
+// export const url =  'http://lamisplus.org:7090/api/v1/';
+// export const  token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjkxMzMzMTI3fQ.cKbExk5j3i16nlSlIFXtUk53oJXteOPB-1UbZIIDX0ix-Pm5U8nbc0_1gyT8my8XI-wH6T-f26Doy3AFFvckfw';
