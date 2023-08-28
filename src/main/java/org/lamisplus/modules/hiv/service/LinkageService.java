384f312af4621fae1a20946a6aa0d84a4b45fbac
aniwange33 <terteseamos@gmail.com>
2023-08-28 13:18:51 +0100

    fix: rework service and controller

--- src/main/java/org/lamisplus/modules/hiv/service/LinkageService.java
+++ src/main/java/org/lamisplus/modules/hiv/service/LinkageService.java
@@ -2,14 +2,13 @@ package org.lamisplus.modules.hiv.service;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.type.CollectionType;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.compress.utils.IOUtils;
-import org.lamisplus.modules.hiv.domain.entity.Linkage;
+import org.lamisplus.modules.hiv.domain.entity.OvcLinkage;
 import org.lamisplus.modules.hiv.repositories.LinkageRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -35,13 +34,13 @@ public class LinkageService {
     SimpleDateFormat df = new SimpleDateFormat("yyyyMMddHHmm");
     private static final String LINKAGE_EXPORT = System.getProperty("user.dir") + File.separator + "export" + File.separator + "linkage";
     private static final String LINKAGE_IMPORT = System.getProperty("user.dir") + File.separator + "import" + File.separator + "linkage";
-
-    public List<Linkage> save(List<Linkage> entities) {
-        List<Linkage> updateLinkage = new ArrayList<>();
-        for (Linkage entity : entities) {
-            Optional<Linkage> optionalLinkage = repository.findByOvcUniqueId(entity.getOvcUniqueId());
+    
+    public List<OvcLinkage> save(List<OvcLinkage> entities) {
+        List<OvcLinkage> updateLinkage = new ArrayList<>();
+        for (OvcLinkage entity : entities) {
+            Optional<OvcLinkage> optionalLinkage = repository.findByOvcUniqueId(entity.getOvcUniqueId());
             if (optionalLinkage.isPresent()) {
-                Linkage update = optionalLinkage.get();
+                OvcLinkage update = optionalLinkage.get();
                 entity.setId(update.getId());
                 entity.setHouseholdUniqueId(update.getHouseholdUniqueId());
             } else {
@@ -50,39 +49,37 @@ public class LinkageService {
             updateLinkage.add(entity);
         }
         entities.clear();
-       for(Linkage linkage :updateLinkage){
-	       return repository.saveAll(updateLinkage);
-       }
-	   return updateLinkage;
+        
+        return repository.saveAll(updateLinkage);
     }
-
-    public Optional<Linkage> findById(UUID id) {
+    
+    public Optional<OvcLinkage> findById(UUID id) {
         return repository.findById(id);
     }
-
-    public List<Linkage> findAll() {
-        return repository.findAll().stream().sorted(Comparator.comparing(Linkage::getDateModified).reversed()).collect(Collectors.toList());
+    
+    public List<OvcLinkage> findAll() {
+        return repository.findAll().stream().sorted(Comparator.comparing(OvcLinkage::getLastModifiedDate).reversed()).collect(Collectors.toList());
     }
-
-    public Linkage update(Linkage entity, UUID id) {
+    
+    public OvcLinkage update(OvcLinkage entity, UUID id) {
         entity.setId(id);
-
+        
         return repository.save(entity);
     }
-
-    public List<Linkage> findByDatimCode(String datimCode) {
+    
+    public List<OvcLinkage> findByDatimCode(String datimCode) {
         return repository.findByDatimCode(datimCode);
     }
-
-    public Optional<Linkage> findByArtNumber(String artNumber) {
+    
+    public Optional<OvcLinkage> findByArtNumber(String artNumber) {
         return repository.findByArtNumber(artNumber);
     }
-
-    public Optional<Linkage> findByOvcUniqueId(String ovcUniqueId) {
+    
+    public Optional<OvcLinkage> findByOvcUniqueId(String ovcUniqueId) {
         return repository.findByOvcUniqueId(ovcUniqueId);
     }
-
-    public List<Linkage> findByHouseholdUniqueId(String householdUniqueId) {
+    
+    public List<OvcLinkage> findByHouseholdUniqueId(String householdUniqueId) {
         return repository.findByHouseholdUniqueId(householdUniqueId);
     }
 
@@ -90,9 +87,10 @@ public class LinkageService {
 //        String filePath = "None";
 //        try {
 //            //write your query to retrieve vc from database
-//            List<LinkageRequest> linkageList = getAllPositiveNonArt();
+//            //List<LinkageRequest> linkageList = getAllPositiveNonArt();
+//            List<LinkageRequest> linkageList = new ArrayList<>();
 //            if (!linkageList.isEmpty()) {
-//                filePath = LINKAGE_EXPORT + File.Separator + "ovc_linkage_" + df.format(new java.util.Date()) + ".json";
+//                filePath = LINKAGE_EXPORT + File.separator + "ovc_linkage_" + df.format(new java.util.Date()) + ".json";
 //                String json = objectToJson(linkageList);
 //                writeObjectToFile(json, filePath);
 //                File file = new File(filePath);
@@ -109,25 +107,28 @@ public class LinkageService {
 //
 //        return filePath;
 //    }
-
+    
     public boolean importJsonFromFile(MultipartFile file) throws IOException {
         boolean result = false;
         String jsonObjectFilePath = LINKAGE_IMPORT + File.separator + file.getOriginalFilename();
+        log.info("file path: " + jsonObjectFilePath);
         file.transferTo(new File(jsonObjectFilePath));
-        List<Linkage> linkageList = jsonFileToObjectList(jsonObjectFilePath);
+        List<OvcLinkage> linkageList = jsonFileToObjectList(jsonObjectFilePath);
+        log.info("linked list: " + linkageList);
         linkageList = save(linkageList);
         if (!linkageList.isEmpty()) {
             result = true;
         }
+        
         return result;
     }
-
+    
     @PostConstruct
     public void init() {
         createDirectoryIfNotExists(LINKAGE_EXPORT);
         createDirectoryIfNotExists(LINKAGE_IMPORT);
     }
-
+    
     public Set<String> listFilesUsingDirectoryStream() throws IOException {
         final Set<String> fileList = new HashSet<>();
         try (DirectoryStream<Path> stream = Files.newDirectoryStream(Paths.get(LINKAGE_EXPORT))) {
@@ -137,10 +138,10 @@ public class LinkageService {
                 }
             }
         }
-
+        
         return fileList;
     }
-
+    
     @SneakyThrows
     public ByteArrayOutputStream downloadFile(String fileName) {
         ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
@@ -148,13 +149,13 @@ public class LinkageService {
                 .filter(f -> f.equals(fileName))
                 .findFirst();
         fileToDownload.ifPresent(s -> {
-            try (InputStream is = Files.newInputStream(Paths.get(directory + s))) {
+            try (InputStream is = Files.newInputStream(Paths.get(s))) {
                 IOUtils.copy(is, byteArrayOutputStream);
             } catch (IOException exception) {
                 log.debug("An error occurred while downloading file: {}", exception.getMessage());
             }
         });
-
+        
         return byteArrayOutputStream;
     }
 
@@ -162,48 +163,49 @@ public class LinkageService {
 //        throw new LinkageNotFoundException("Linkage Not Found with ID: " + value);
 //    }
 
-    private String objectToJson(Object object) {
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
-        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-        objectMapper.registerModule(new JavaTimeModule());
-        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-        objectMapper.configure(DeserializationFeature.FAIL_ON_NULL_FOR_PRIMITIVES, false);
-        objectMapper.configure(DeserializationFeature.FAIL_ON_NUMBERS_FOR_ENUMS, false);
-        try {
-            if (object != null) {
-                return objectMapper.writeValueAsString(object);
-            }
-            return null;
-        } catch (IOException e) {
-            log.error("Error writing object to JSON");
-            return null;
-        }
-    }
-
+//    private String objectToJson(List<LinkageRequest> object) {
+//        ObjectMapper objectMapper = new ObjectMapper();
+//        objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
+//        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+//        objectMapper.registerModule(new JavaTimeModule());
+//        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+//        objectMapper.configure(DeserializationFeature.FAIL_ON_NULL_FOR_PRIMITIVES, false);
+//        objectMapper.configure(DeserializationFeature.FAIL_ON_NUMBERS_FOR_ENUMS, false);
+//        try {
+//            if (object != null) {
+//                return mapper.writeValueAsString(object);
+//            }
+//            return null;
+//        } catch (IOException e) {
+//            log.error("Error writing object to JSON");
+//            return null;
+//        }
+//    }
+    
     private void writeObjectToFile(String jsonObject, String jsonFilePath) throws IOException {
         try(FileWriter writer = new FileWriter(jsonFilePath)){
             writer.write(jsonObject);
         } catch (IOException exception) {
             log.debug("Exception: {}", exception.getMessage());
         }
-
+        
     }
-
-    private List<Linkage> jsonFileToObjectList(String jsonFilePath){
+    
+    private List<OvcLinkage> jsonFileToObjectList(String jsonFilePath){
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.registerModule(new JavaTimeModule());
         objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
         objectMapper.configure(DeserializationFeature.FAIL_ON_NULL_FOR_PRIMITIVES, false);
         objectMapper.configure(DeserializationFeature.FAIL_ON_NUMBERS_FOR_ENUMS, false);
-
-        List<Linkage> objectList = new ArrayList<>();
+        List<OvcLinkage> objectList = new ArrayList<>();
         try {
             if (jsonFilePath != null) {
+                
+                Path  path = Paths.get("json");
                 File file = new File(jsonFilePath);
                 if (file.exists()) {
-                    CollectionType javaType = objectMapper.getTypeFactory()
-                            .constructCollectionType(List.class, Linkage.class);
+                    CollectionType javaType =
+                            objectMapper.getTypeFactory().constructCollectionType(List.class, OvcLinkage.class);
                     objectList = objectMapper.readValue(file, javaType);
                 } else {
                     log.error("File " + jsonFilePath + " does not exist");
@@ -213,10 +215,10 @@ public class LinkageService {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
+        
         return objectList;
     }
-
+    
     private void createDirectoryIfNotExists(String directoryPath) {
         File directory = new File(directoryPath);
         if (!directory.exists()) {
@@ -227,5 +229,5 @@ public class LinkageService {
             }
         }
     }
-
+    
 }
