1f2b8e29259d824adef70a78f97ece5040b2691d
vixxjy <ajorvictor48@gmail.com>
2023-09-16 10:39:51 +0100

    [HIV] TPT TB flow updated

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -1,6 +1,8 @@
-//export const  token = (new URLSearchParams(window.location.search)).get("jwt")
-//export const url = '/api/v1/'
+export const  token = (new URLSearchParams(window.location.search)).get("jwt")
+export const url = '/api/v1/'
 
+/*
 export const url = "http://localhost:8383/api/v1/";
 export const token =
   "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjk0ODk1MDA1fQ._3qmJ02xpT8DRlJMEHzox4kMRjVVLygy9TrZSq8MKjaxAzodVqFLYuD1apIMt4k1RNA8LfLKLG9XSzSV4t_aqA";
+*/
