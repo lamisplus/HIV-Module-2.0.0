a3e37767baea6120d0a574ac74ddf9b580639488
vixxjy <ajorvictor48@gmail.com>
2023-10-04 09:29:07 +0100

    [HIV] biometric status on tracking form added

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -1,7 +1,6 @@
 export const token = new URLSearchParams(window.location.search).get("jwt");
 export const url = "/api/v1/";
 
-// export const url = "http://localhost:8789/api/v1/";
-// export const token =
-//   "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjk2MzcwNDM3fQ.8kw8IOOdpzL0pyYwsgRHR2wsQ13mnSDzqhGbHP3s1C4GqNDyYEb64aaAgXzOr567aeKGF160krXqLAFTQJkaIQ";
-//
+//export const url = "http://localhost:8383/api/v1/";
+//export const token =
+//  "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjk2NDUwODM3fQ.iB_tZks1MSsG23HW35lf-RrFwTRlK3PTJSWi9annywXzrFW4rZxYIZtw2ZT9PwX0HDkczsNRbd0j6dxvBBfknA";
