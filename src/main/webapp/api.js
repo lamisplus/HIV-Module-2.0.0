9adb6875cfbd188d85518dc3687871122e912b43
vixxjy <ajorvictor48@gmail.com>
2023-10-17 10:14:41 +0100

    [HIV] care and support added to the history page

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -3,4 +3,4 @@ export const url = "/api/v1/";
 
 //export const url = "http://localhost:8383/api/v1/";
 //export const token =
-//  "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjk2NDYyMzMzfQ.vJSR0RHxRRCra-BmJmecGqe61-dg7XpqsYVNSxQL7cxYoJPR4gSHxkrxMVyRVJUujHKLbdXGIpSmr988DcS1PA";
+//  "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjk3NTc2MDk2fQ.GJQ5uYRnrVH1f7Uld2gQsocbN0nHhIVFMuujS_NHMjiZi5AxN0FIynBoMqO7wLy2adnH8b5Rc89bA71Ey52_gg";
