6d7690adb73f69acee0d1392201f1ce09c08e1fa
Mathew Adegbite <mathewadegbite@gmail.com>
2023-10-25 12:30:20 +0100

    Fix Tracking form not saving

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -1,5 +1,5 @@
 export const token = new URLSearchParams(window.location.search).get("jwt");
 export const url = "/api/v1/";
-//export const url = "http://localhost:8383/api/v1/";
-//export const token =
-  //"eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjk3NzU4MTIwfQ.snT0wsUHMBu6fVNQaecW_YrB6-_oxZS0kvMEMQl84BuY_AGiJqhNa-5UHWcM-iopaQTDaqwu-QGq4T3_DQc2JA";
+// export const url = "http://localhost:8789/api/v1/";
+// export const token =
+//   "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjk4MjU2MzU1fQ.qo1ianoBMhCHedl6-r7MGXB9da3cXu8PlEAFCfK8P07PgLvaa4A67xanzZWmi1ilaTg3A5JVpR09pOdM4QQKGw";
