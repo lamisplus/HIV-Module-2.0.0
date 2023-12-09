ae984cdca05b4f06fa766de72ff9203d5a9869b0
vixxjy <ajorvictor48@gmail.com>
2023-12-09 08:00:52 +0100

    [HIV] updates on redacted reason implementation

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -1,6 +1,6 @@
-// export const token = new URLSearchParams(window.location.search).get("jwt");
-// export const url = "/api/v1/";
-export const url = "http://localhost:8383/api/v1/";
+export const token = new URLSearchParams(window.location.search).get("jwt");
+export const url = "/api/v1/";
+//export const url = "http://localhost:8383/api/v1/";
 // export const url = "http://lamisplus.org:7090/api/v1/";
-export const token =
-  "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzAxMzY4ODc1fQ.XZk-3fLK7sDwjAvZaLEiBJACLe29xYNLhYMbARCRSP-0vkFodx3roSEgKkWlwQFx65m3Eo0n67Lor4CoTwAlmg";
+//export const token =
+  //"eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzAxMzY4ODc1fQ.XZk-3fLK7sDwjAvZaLEiBJACLe29xYNLhYMbARCRSP-0vkFodx3roSEgKkWlwQFx65m3Eo0n67Lor4CoTwAlmg";
