f86a7fe2442feba8e16396f5d4ce5dd83e6f6bce
Mathew Adegbite <mathewadegbite@gmail.com>
2023-12-15 17:16:02 +0100

    fixing the verification attempt status and ongoing

--- src/main/webapp/jsx/components/ClientVerfication/Index.js
+++ src/main/webapp/jsx/components/ClientVerfication/Index.js
@@ -241,6 +241,7 @@ const Tracking = (props) =>{
     }
     const addAttempt = e => {
         //attempt.anyOfTheFollowing=selected
+        attempt.outcome = attempt.verificationStatus !=="" && attempt.verificationStatus ==="Verification Ongoing" ? "Verification Ongoing" : attempt.outcome;
         attempt.verificationAttempts = selectedOptions.join(', ');
         if(validateAttempt()){ 
             setAttemptList([...attemptList, attempt])
@@ -487,14 +488,10 @@ const Tracking = (props) =>{
                                         value={attempt.verificationStatus}  
                                     >
                                         <option value=""> Select</option>
-                                        <option value="verificationOngoing">Verification ongoing </option>
-                                        <option value="recordsDiscontinuedArchived"> Records Discontinued & Archived    </option>
-                                        <option value="recordsVerified">Records Verified</option>
-                                        {optionsForCallOutCome.map((value) => (
-                                        <option key={value.code} value={value.display}>
-                                            {value.display}
-                                        </option>   
-                                    ))}
+                                        <option value="Verification Ongoing">Verification ongoing </option>
+                                        <option value="Records Discontinued Archived"> Records Discontinued & Archived    </option>
+                                        <option value="Records Verified">Records Verified</option>
+
                                     </Input>
                                     {errors.verificationStatus !=="" ? (
                                     <span className={classes.error}>{errors.verificationStatus}</span>
@@ -502,36 +499,58 @@ const Tracking = (props) =>{
                                 </FormGroup>
                                 </div>
                             </div>
-                            <div className="form-group mb-3 col-md-3">
-                                <FormGroup>
-                                <Label > Outcome <span style={{ color:"red"}}> *</span></Label>
-                                <Input
-                                    type="select"
-                                    name="outcome"
-                                    id="outcome"
-                                    value={attempt.outcome}
-                                    onChange={handleInputChangeAttempt}
-                                    style={{border: "1px solid #014D88", borderRadius:"0.25rem"}}
+                            {attempt.verificationStatus ==="Verification Ongoing" ? (<>
+                                <div className="form-group mb-3 col-md-3">
+                                    <FormGroup>
+                                    <Label > Outcome <span style={{ color:"red"}}> *</span></Label>
+                                    <Input
+                                        type="select"
+                                        name="outcome"
+                                        id="outcome"
+                                        value={attempt.outcome}
+                                        onChange={handleInputChangeAttempt}
+                                        style={{border: "1px solid #014D88", borderRadius:"0.25rem"}}
 
-                                >
-                                        <option value=""> Select </option>
-                                        <option value="valid"> Valid </option>
-                                        <option value="invalid"> invalid</option>  
-                                        {optionsForCallOutCome.map((value) => (
-                                        <option key={value.code} value={value.display}>
-                                            {value.display}
-                                        </option>
-                                        
-                                    ))}
-                                    </Input>
-                                {errors.outcome !=="" ? (
-                                <span className={classes.error}>{errors.outcome}</span>
-                                ) : "" }
-                                </FormGroup>
-                            </div>
+                                    >
+                                            <option value="Verification Ongoing"> Verification Ongoing </option>
+
+                                        </Input>
+                                    {errors.outcome !=="" ? (
+                                    <span className={classes.error}>{errors.outcome}</span>
+                                    ) : "" }
+                                    </FormGroup>
+                                </div>
+                                </>):
+                                (<>
+                                    <div className="form-group mb-3 col-md-3">
+                                        <FormGroup>
+                                        <Label > Outcome <span style={{ color:"red"}}> *</span></Label>
+                                        <Input
+                                            type="select"
+                                            name="outcome"
+                                            id="outcome"
+                                            value={attempt.outcome}
+                                            onChange={handleInputChangeAttempt}
+                                            style={{border: "1px solid #014D88", borderRadius:"0.25rem"}}
+
+                                        >
+                                                <option value=""> Select </option>
+                                                <option value="valid"> Valid </option>
+                                                <option value="invalid"> invalid</option>
+
+                                            </Input>
+                                        {errors.outcome !=="" ? (
+                                        <span className={classes.error}>{errors.outcome}</span>
+                                        ) : "" }
+                                        </FormGroup>
+                                    </div>
+                            </>)
+
+
+                            }
                             <div className="form-group mb-3 col-md-4">
                                 <FormGroup>
-                                <Label > Comment <span style={{ color:"red"}}> *</span></Label>
+                                <Label > Comment</Label>
                                 <Input
                                     type="text"
                                     name="comment"
@@ -601,7 +620,7 @@ const Tracking = (props) =>{
                                         <option value="Yes"> Yes </option>
                                         <option value="No"> No </option>
 
-                                    ))}
+                                  
                                     </Input>
                                 {errors.discontinuation !=="" ? (
                                 <span className={classes.error}>{errors.discontinuation}</span>
