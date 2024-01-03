809f821e23681df7f65b671ef9d66d84a4c50c8f
Tech teni <yakubganiyat96@gmail.com>
2024-01-03 15:45:09 +0100

    submit the trqansfer form

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -1,5 +1,5 @@
-export const token = new URLSearchParams(window.location.search).get("jwt");
-export const url = "/api/v1/";
-// export const url = "http://localhost:8789/api/v1/";
-// export const token =
-// "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzAyNTYxNzU1fQ.I7PfsCLy6xmLffslXCOJqI2KxtNJxpqTcJvFi83xRnDi_2mR7J464MIiJi_zAO61T44C6T4OP2fHWiw9ajQ6UA";
+// export const token = new URLSearchParams(window.location.search).get("jwt");
+// export const url = "/api/v1/";
+export const url = "http://localhost:8383/api/v1/";
+export const token =
+  "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzA0Mjk4NDE1fQ.K7r3aFFTlztGVCu-I2B1FR_RJcLK2jGpfc2TDTgUHYUAAtxN0oBJ39CYfppnaAkTnpRFOO_YcYxba4Fo4Xg4CQ";
