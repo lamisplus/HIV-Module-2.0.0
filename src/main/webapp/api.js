77cd379864b5602800c3aaa232e0ddd35857970d
vixxjy <ajorvictor48@gmail.com>
2024-02-06 15:25:28 +0100

    [HIV] updates on tracking form not updating hiv status-latest

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -1,5 +1,5 @@
-export const token = new URLSearchParams(window.location.search).get("jwt");
-export const url = "/api/v1/";
-//export const url = "http://localhost:8383/api/v1/";
-//export const token =
-//"eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzAyNjkxNjA5fQ.MPDsrMvSArWNYtU9sQBMVfIMD-3A6kBXFQIfHagW_JE5QCCZntBMxaL4raOBA54zWoMxDk5EWbxMCIrywlMnpQ";
+//export const token = new URLSearchParams(window.location.search).get("jwt");
+//export const url = "/api/v1/";
+export const url = "http://localhost:8383/api/v1/";
+export const token =
+  "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzA3MjY4NTY2fQ.vRlLYQMi3nv0Lj3_bsNoDdp_TNuzxY5UdPNYxG6kQR61jBW2NEfih8IcaG67cvfCYs2yFnTe94UQGLpjwRmqrw";
