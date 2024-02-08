c14ef7155ce19329eb7cc2d8b98766de7e159160
Tech teni <yakubganiyat96@gmail.com>
2024-02-08 13:10:34 +0100

    change the token frok local to urlParams

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -1,5 +1,5 @@
-// export const token = new URLSearchParams(window.location.search).get("jwt");
-// export const url = "/api/v1/";
-export const url = "http://localhost:8383/api/v1/";
-export const token =
-  "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzA3NDE0ODE4fQ.0jTLR9a2cHBldqGr9AgUu8yh1_5-oZeZKfuFkXmEDkIFEAejI1rZqQn5ApVaTFdvRjRN_w5zWhtXDioQWP4h_g";
+export const token = new URLSearchParams(window.location.search).get("jwt");
+export const url = "/api/v1/";
+// export const url = "http://localhost:8383/api/v1/";
+// export const token =
+//   "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzA3NDE0ODE4fQ.0jTLR9a2cHBldqGr9AgUu8yh1_5-oZeZKfuFkXmEDkIFEAejI1rZqQn5ApVaTFdvRjRN_w5zWhtXDioQWP4h_g";
