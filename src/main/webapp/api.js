b323cd2d218149b45f8c9879779b804033c28e33
vixxjy <ajorvictor48@gmail.com>
2024-02-08 13:35:34 +0100

    [HIV] updates on multiple pool connection and shutting down db

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -1,5 +1,5 @@
-export const token = new URLSearchParams(window.location.search).get("jwt");
-export const url = "/api/v1/";
-//export const url = "http://localhost:8383/api/v1/";
-//export const token =
-  //"eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzA3MjY4NTY2fQ.vRlLYQMi3nv0Lj3_bsNoDdp_TNuzxY5UdPNYxG6kQR61jBW2NEfih8IcaG67cvfCYs2yFnTe94UQGLpjwRmqrw";
+//export const token = new URLSearchParams(window.location.search).get("jwt");
+//export const url = "/api/v1/";
+export const url = "http://localhost:8383/api/v1/";
+export const token =
+  "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzA3NDIwODIxfQ.92ufcosL16KVa20vh3pn78djSyWKGZkPWkjPP8VLappHYRfmdU_6mdQdy6dW7Csl82R9c4tcizgZbUfX30O5WQ";
