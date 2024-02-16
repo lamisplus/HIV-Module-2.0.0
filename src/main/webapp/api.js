97a203062363efff4ec085c6fe9e90c021aa9d67
Emmanuel Chinonye Nnajiofor <emma.nnajichi@gmail.com>
2024-02-16 09:09:03 +0100

    minor hanges to resolve test sellect not resolving to default

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -1,9 +1,9 @@
- export const token = new URLSearchParams(window.location.search).get("jwt");
- export const url = "/api/v1/";
+//  export const token = new URLSearchParams(window.location.search).get("jwt");
+//  export const url = "/api/v1/";
 
 
 
-// export const url = "http://localhost:8383/api/v1/";
-// export const token =
-// "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzA3OTI5ODY5fQ.hUSUu7rTPRmkh0i_LjjTFojy-7Cy23TB5zSssY0KhUmGxZg59TkNzrtjfqwNnd11nH-4ZKuxKC2xG1yHLH6p3g";
+export const url = "http://localhost:8383/api/v1/";
+export const token =
+"eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzA4MDMyOTQ3fQ.LWXQ40ODT8CsW81i2aq4MHLqY0J6Q2v-ZLvAVXIW86VEr6n9Yr3_sScGiHSLXVSgQFuAuQl782un2e8ylQZU6Q";
 
