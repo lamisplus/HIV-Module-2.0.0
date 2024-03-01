8887d28f29a8802563e4861afd25ab1f9bd834b2
Anana Aristotle <30607727+legendaryA3@users.noreply.github.com>
2024-03-01 19:08:30 +0100

    Add change set for newly add codeset records, fix blank screen issue when you select switch regimen on EAC outcome, and also fixed null value been safe during update on EAC session

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -1,8 +1,8 @@
 export const url =
   process.env.NODE_ENV === "development"
-    ? "http://localhost:8789/api/v1/"
+    ? "http://localhost:8383/api/v1/"
     : "/api/v1/";
 export const token =
   process.env.NODE_ENV === "development"
-    ? "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzA4OTY1MDA4fQ.-n-Sl3haAPlPP6eQrxvwX6DNONsN25Hrk0iZJkro-uWey-q-kv1cL6fUpzpgJiaHqPXXsHTJCNJrvnD5wgZYjg"
+    ? "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzA5MjM4NDE5fQ.Af8JDvM5SHqiLVSYqFeT2qkDERRCGcpFKyftoSlgxOnH78EDFg6lbVAAIs_mFSFCwHlYnUNfOb4Wry2YeMj1tg"
     : new URLSearchParams(window.location.search).get("jwt");
