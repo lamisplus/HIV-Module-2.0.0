071c7227f8b7afb0a249d0011d0bae836f40ed26
vixxjy <ajorvictor48@gmail.com>
2024-03-15 18:11:35 +0100

    [HIV] api updates

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -1,5 +1,10 @@
-export const token = new URLSearchParams(window.location.search).get("jwt");
-export const url = "/api/v1/";
-//export const url = "http://localhost:8383/api/v1/";
-//export const token =
-//"eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzEwNTY1NTYyfQ.GnumzdyIKTGMTAWQ0rd3edHvbHKKi0dslkldKRLi0wcQQFfVmyQoh7O5ImEAdkddqA125kfrCvLNRYgnORHWmg";
+
+export const url =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:8789/api/v1/"
+    : "/api/v1/";
+export const token =
+  process.env.NODE_ENV === "development"
+    ? "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzEwNTM3MTkzfQ.FTpuhKTLwusTsps76ISIuqFTD_uCXN1vfqnIPdobEgaO3VgrmG4X-438hf8VUgIk_oix9QbkMmSwwIKP6e1cNg"
+    : new URLSearchParams(window.location.search).get("jwt");
+
