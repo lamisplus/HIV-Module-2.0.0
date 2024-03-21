4208990d99c729bcdc63134a4f78d1c87184ddc0
Aniwange Tertese Amos <terteseamos@gmail.com>
2024-03-21 12:43:29 +0100

    Fix: - Added TDF(300mg)+3TC(300mg)+DTG(50mg) to pediatric 1st line. - Aligned the serviceProvided field of DsdDevolvement.java with column serviceProvider in the dsd_devolvement table to resolve the display issue of devolvement activity.

--- src/main/java/org/lamisplus/modules/hiv/service/RegimenService.java
+++ src/main/java/org/lamisplus/modules/hiv/service/RegimenService.java
@@ -8,42 +8,49 @@ import org.lamisplus.modules.hiv.repositories.RegimenRepository;
 import org.lamisplus.modules.hiv.repositories.RegimenTypeRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class RegimenService {
-	
-	private final RegimenRepository regimenRepository;
-	private final RegimenTypeRepository regimenTypeRepository;
-	
-	private final HIVDrugRepository hivDrugRepository;
-	
-	
-	public List<Regimen> getRegimenByTypeId(Long typeId) {
-		RegimenType regimenType = regimenTypeRepository
-				.findById(typeId)
-				.orElseThrow(() -> new EntityNotFoundException(RegimenType.class, "id ", String.valueOf(typeId)));
-		return regimenRepository.getAllByRegimenTypeAndActiveIsTrue(regimenType);
-	}
-	
-	public Regimen getRegimenById(Long id) {
-		return regimenRepository.findById(id).orElseThrow(() -> new EntityNotFoundException(Regimen.class, "id", Long.toString(id)));
-	}
-	
-	public List<HIVDrug> getRegimenDrugsById(Long id) {
-		Regimen regimen = regimenRepository.findById(id).orElseThrow(() -> new EntityNotFoundException(Regimen.class, "id", Long.toString(id)));
-		List<RegimenDrug> regimenDrugs = regimen.getRegimenDrugs();
-		return regimenDrugs.stream()
-				.map(regimen1 -> {
-					HIVDrug drug = regimen1.getDrug();
-					Long drugId = 0L;
-					if (drug != null) {
-						drugId = drug.getId();
-					}
-					return hivDrugRepository.findById(drugId).orElse(null);
-				})
-				.collect(Collectors.toList());
-	}
+
+    private final RegimenRepository regimenRepository;
+    private final RegimenTypeRepository regimenTypeRepository;
+
+    private final HIVDrugRepository hivDrugRepository;
+
+
+    public List<Regimen> getRegimenByTypeId(Long typeId) {
+        RegimenType regimenType = regimenTypeRepository
+                .findById(typeId)
+                .orElseThrow(() -> new EntityNotFoundException(RegimenType.class, "id ", String.valueOf(typeId)));
+        List<Regimen> regimens = regimenRepository.getAllByRegimenTypeAndActiveIsTrue(regimenType);
+        if (regimenType.getId( ) !=  null  &&  regimenType.getId() == 3L) {
+            Optional<Regimen> adultRegimen = regimenRepository.findById(116L);
+            adultRegimen.ifPresent(regimens::add);
+        }
+        return regimens;
+    }
+
+    public Regimen getRegimenById(Long id) {
+        return regimenRepository.findById(id).orElseThrow(() -> new EntityNotFoundException(Regimen.class, "id", Long.toString(id)));
+    }
+
+    public List<HIVDrug> getRegimenDrugsById(Long id) {
+        Regimen regimen = regimenRepository.findById(id).orElseThrow(() -> new EntityNotFoundException(Regimen.class, "id", Long.toString(id)));
+        List<RegimenDrug> regimenDrugs = regimen.getRegimenDrugs();
+        return regimenDrugs.stream()
+                .map(regimen1 -> {
+                    HIVDrug drug = regimen1.getDrug();
+                    Long drugId = 0L;
+                    if (drug != null) {
+                        drugId = drug.getId();
+                    }
+                    return hivDrugRepository.findById(drugId).orElse(null);
+                })
+                .collect(Collectors.toList());
+    }
 }
