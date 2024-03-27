ca96d1117924cf8555d81f18433f89b6add0c6b0
Basil <ohuruogubasil@gmail.com>
2024-03-27 08:58:19 +0100

    fix:update-function

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -5,6 +5,6 @@ export const url =
     : "/api/v1/";
 export const token =
   process.env.NODE_ENV === "development"
-    ? "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzExNDcwMzg1fQ.6aU7XLGuMfifd5jpmgDZsAWzypaQjvsMjEwSgvJneywsdgH8O5OpA3BcAIdWmc-U_DzDplHeAYV5XBElVnD1iA"
+    ? "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzExNTQ2NTU1fQ.0okVv7_kS0Hz0-pw0_SC4WH24j_lJ46_yHrZEYPmsKahhTOiuPHN4kP8Eo-BfQQAzDHtEdId-ZtTGtPQepwu3A"
     : new URLSearchParams(window.location.search).get("jwt");
 
