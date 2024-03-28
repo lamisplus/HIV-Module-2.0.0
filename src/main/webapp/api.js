b5ac769c4d12bd0c1450dd90eb3b2016f992838a
Basil <ohuruogubasil@gmail.com>
2024-03-28 14:51:44 +0100

    added:validation ovc import

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -5,6 +5,6 @@ export const url =
     : "/api/v1/";
 export const token =
   process.env.NODE_ENV === "development"
-    ? "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzExNTQ2NTU1fQ.0okVv7_kS0Hz0-pw0_SC4WH24j_lJ46_yHrZEYPmsKahhTOiuPHN4kP8Eo-BfQQAzDHtEdId-ZtTGtPQepwu3A"
+    ? "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzExNjQ5MTE1fQ.oqw_3nLo2t3SNokaYEl4cwx62IeCt9fdwOnUj798DR6dGwRIw6iNhfHVjhZ9toRL8dj2QvjmqeVHHtJbsq9KQQ"
     : new URLSearchParams(window.location.search).get("jwt");
 
