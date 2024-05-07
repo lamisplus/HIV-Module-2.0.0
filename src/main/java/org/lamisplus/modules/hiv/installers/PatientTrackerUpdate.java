cb450df0e181da89095db21682ad4b71a76c2eed
mechack2022 <dtoms624@gmail.com>
2024-05-07 08:54:06 +0100

    fix:not auto populating date_of_date va_cause_of_death, cause_of_death on view an update

--- src/main/java/org/lamisplus/modules/hiv/installers/PatientTrackerUpdate.java
+++ src/main/java/org/lamisplus/modules/hiv/installers/PatientTrackerUpdate.java
@@ -7,7 +7,7 @@ import org.springframework.core.annotation.Order;
 @Order(8)
 @Installer(name = "patient-tracker-update-installer",
         description = "Update patient tracker table",
-        version = 2)
+        version = 3)
 public class PatientTrackerUpdate extends AcrossLiquibaseInstaller {
     public  PatientTrackerUpdate() {
         super ("classpath:installers/hiv/schema/patient-tracker-update.xml");
--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -5,6 +5,5 @@ export const url =
     : "/api/v1/";
 export const token =
   process.env.NODE_ENV === "development"
-    ? "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzEyMDcyMjAzfQ.RmqylQRMwNVmbSLctK9UeAQ4XzfxUpiRkXAmVjvmb7B8i5C6cnNyZnsJSeMK4sUh8OAJ1R8WATqv2dfKlF4KWg"
-    : new URLSearchParams(window.location.search).get("jwt");
+? "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzE1MDMyNjk4fQ.XuwAbzO12czYcKAUBSuPk7OEDxtPEMBrOqWAm_NFomTDdJy9wICYdI6E7ypG0XP_GFLFgpp8S7TQfPWyGpyvIw"    : new URLSearchParams(window.location.search).get("jwt");
 
