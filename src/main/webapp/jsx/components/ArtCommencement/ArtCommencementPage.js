022c4a327c04892f23f2d04c3fe57693bfaafe4b
Gamey001 <87187530+Gamey001@users.noreply.github.com>
2024-06-14 09:32:54 +0100

    Update ArtCommencementPage.js

--- src/main/webapp/jsx/components/ArtCommencement/ArtCommencementPage.js
+++ src/main/webapp/jsx/components/ArtCommencement/ArtCommencementPage.js
@@ -228,7 +228,7 @@ const ArtCommencement = (props) => {
       })
       .then((response) => {
         const artRegimenChildren = response.data.filter(
-          (x) => x.id === 3 || x.id === 4 
+          (x) => x.id === 3 || x.id === 4
         );
         setChildRegimenLine(artRegimenChildren);
       })
@@ -574,7 +574,7 @@ const ArtCommencement = (props) => {
                         id="visitDate"
                         onChange={handleInputChange}
                         value={objValues.visitDate}
-                        min="01-01-1980"
+                        // min="01-01-1980"
                         max={moment(new Date()).format("YYYY-MM-DD")}
                         style={{
                           border: "1px solid #014D88",
@@ -605,7 +605,6 @@ const ArtCommencement = (props) => {
                         id="visitDate"
                         onChange={handleInputChange}
                         value={objValues.visitDate}
-                        min={enrollDate}
                         max={moment(new Date()).format("YYYY-MM-DD")}
                         style={{
                           border: "1px solid #014D88",
