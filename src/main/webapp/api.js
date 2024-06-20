eee7d115a9aee9b48d908f20a0ccc6585f709c1c
Mathew Adegbite <mathewadegbite@gmail.com>
2024-06-20 16:37:19 +0100

    pushing tpt updated code

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -5,5 +5,5 @@ export const url =
     : "/api/v1/";
 export const token =
   process.env.NODE_ENV === "development"
-? "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzE4ODIzNDg3fQ.ye30FQKorAstmsVaJQr09V1I3I4O6ZbqM02xa57AmOwSwxPRtO9UmXOzKygTUxVTRxpSbxPDme07kOcNrT4PDg"   : new URLSearchParams(window.location.search).get("jwt");
+? "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzE4ODk2MjE5fQ.wD4FpnHUqby0Zp0gXdb_FSECOJyj9qO1pSZwnJbrSQsxIA0_PXQeGLLVbtTIjMeidFKSKkNCt3PlsRlu4N_W0A"   : new URLSearchParams(window.location.search).get("jwt");
 
