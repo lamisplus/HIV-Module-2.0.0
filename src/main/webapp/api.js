bfb99dda3883f01e3a482cf39570cf7557ef34a8
gboyeguntaiwo <dtoms624@gmail.com>
2024-06-22 14:03:04 +0100

    correct MTB detected and not detected options logic

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -4,4 +4,4 @@ export const url =
     : "/api/v1/";
 export const token =
   process.env.NODE_ENV === "development"
-? "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzE4OTYzNDA0fQ.uXlVQSo0U1MDaQJZJYUcNVmRJKuUyLgbNrfQ6as68KeZRC0APoTrT4L5aYS0dEpTreFik3lSbZ1vWWkLRnltAw"  : new URLSearchParams(window.location.search).get("jwt");
+? "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzE5MDQ3NjM2fQ.S9EbgGDVyC6cJMy8uZja_wL2jK_OagOmNPL5-3Tq_ZCEOGtPQvZTPNWrpGh7pP3ovLlqQqa3Dih4hPPlVqxP5w"  : new URLSearchParams(window.location.search).get("jwt");
