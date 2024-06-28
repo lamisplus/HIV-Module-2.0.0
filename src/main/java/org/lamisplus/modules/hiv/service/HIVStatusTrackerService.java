220c853408c282aac392ac5ff5ccade9e63e8434
gboyeguntaiwo <dtoms624@gmail.com>
2024-06-28 15:48:55 +0100

    fix:tracking form is not submitted

--- src/main/java/org/lamisplus/modules/hiv/service/HIVStatusTrackerService.java
+++ src/main/java/org/lamisplus/modules/hiv/service/HIVStatusTrackerService.java
@@ -58,21 +58,42 @@ public class HIVStatusTrackerService {
     private final ApplicationCodesetService applicationCodesetService;
 
 
+//    public HIVStatusTrackerDto registerHIVStatusTracker(HIVStatusTrackerDto hivStatusTrackerDto) {
+//        System.out.println("------------------------------I got to registerHIVStatusTracker method---------------------");
+//        Long personId = hivStatusTrackerDto.getPersonId();
+//        Person existPerson = getPerson(personId);
+//        HIVStatusTracker hivStatusTracker = convertDtoToEntity(hivStatusTrackerDto);
+//        Visit visit = hivVisitEncounter.processAndCreateVisit(personId, hivStatusTrackerDto.getStatusDate());
+//        hivStatusTracker.setVisit(visit);
+//        hivStatusTracker.setArchived(0);
+//        hivStatusTracker.setUuid(UUID.randomUUID().toString());
+//        hivStatusTracker.setAuto(false);
+//        hivStatusTracker.setPerson(existPerson);
+//        log.info("Creating ART Status Starts here");
+//        HIVStatusTracker statusTracker = hivStatusTrackerRepository.save(hivStatusTracker);
+//        log.info("This is Status Tracker " + statusTracker);
+//        return  null;
+//    }
+
     public HIVStatusTrackerDto registerHIVStatusTracker(HIVStatusTrackerDto hivStatusTrackerDto) {
-        System.out.println("------------------------------I got to registerHIVStatusTracker method---------------------");
-        Long personId = hivStatusTrackerDto.getPersonId();
-        Person existPerson = getPerson(personId);
-        HIVStatusTracker hivStatusTracker = convertDtoToEntity(hivStatusTrackerDto);
-        Visit visit = hivVisitEncounter.processAndCreateVisit(personId, hivStatusTrackerDto.getStatusDate());
-        hivStatusTracker.setVisit(visit);
-        hivStatusTracker.setArchived(0);
-        hivStatusTracker.setUuid(UUID.randomUUID().toString());
-        hivStatusTracker.setAuto(false);
-        hivStatusTracker.setPerson(existPerson);
-        log.info("Creating ART Status Starts here");
-        HIVStatusTracker statusTracker = hivStatusTrackerRepository.save(hivStatusTracker);
-        log.info("This is Status Tracker " + statusTracker);
-        return  null;
+        if (hivStatusTrackerDto != null) {
+            System.out.println("------------------------------I got to registerHIVStatusTracker method---------------------");
+            Long personId = hivStatusTrackerDto.getPersonId();
+            Person existPerson = getPerson(personId);
+            HIVStatusTracker hivStatusTracker = convertDtoToEntity(hivStatusTrackerDto);
+            Visit visit = hivVisitEncounter.processAndCreateVisit(personId, hivStatusTrackerDto.getStatusDate());
+            hivStatusTracker.setVisit(visit);
+            hivStatusTracker.setArchived(0);
+            hivStatusTracker.setUuid(UUID.randomUUID().toString());
+            hivStatusTracker.setAuto(false);
+            hivStatusTracker.setPerson(existPerson);
+            log.info("Creating ART Status Starts here");
+            HIVStatusTracker statusTracker = hivStatusTrackerRepository.save(hivStatusTracker);
+            log.info("This is Status Tracker " + statusTracker);
+            return convertEntityToDto(statusTracker); // Convert the entity to DTO and return
+        } else {
+            return new HIVStatusTrackerDto();
+        }
     }
 
 
