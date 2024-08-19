278a80b04b544e558e685ae630bd01a681f28a29
gboyeguntaiwo <dtoms624@gmail.com>
2024-08-19 11:50:01 +0100

    obsersationService dto import corrected

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -4,4 +4,4 @@ export const url =
     : "/api/v1/";
 export const token =
   process.env.NODE_ENV === "development"
-? "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzIzODM3MDYzfQ.hobeqKZMsIhDflnCnFtdzvOlu8MZJAFszm9z_BUQtPrN0eme_yXrff0G984TcXfUR9EWWa2Ezf2HVSK9oV8ruQ"  : new URLSearchParams(window.location.search).get("jwt");
+? "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzI0MDg4OTI3fQ.xEdojzCgsBA9aLcEWdXyMHwUnmho8-4XfpdyK7wDwJXwzulLwx045BV-gmW_HlJZsqk49B2JJNO2J8sZD6hlcQ"  : new URLSearchParams(window.location.search).get("jwt");
