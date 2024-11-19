b04cd57694326ab4e067e0211f1d32711bc6fb00
gboyeguntaiwo <dtoms624@gmail.com>
2024-11-19 13:39:33 +0100

    impl: modified pharmacy refill history to accommodate new UI.

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -4,4 +4,4 @@ export const url =
     : "/api/v1/";
 export const token =
   process.env.NODE_ENV === "development"
-? "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzMxOTQ1MDAzfQ.68jM6k85W2ZZRaPxUILcIorpcBgO2TgSex8clNdm3kOE-SQ1mApIG2A9UzVQgGQZqU8fsuvwmQ3768QtuUcYrA"  : new URLSearchParams(window.location.search).get("jwt");
+? "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzMyMDM1NDgwfQ.a-W4FMAfRwFefjdSTL0FvRb9hMzR83R0cbMq7JctS6EA2coMEHuK5gQXhOxOktfvfl0R7yfjEXtwLCXFguaNqg"  : new URLSearchParams(window.location.search).get("jwt");
