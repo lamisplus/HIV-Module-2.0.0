b817641bbf65b9b0381f2ec0109f05ea5de1b75a
taiwo.gboyegun <taiwo.gboyegun@thepalladiumgroup.com>
2024-12-19 12:31:31 +0100

    jar file build from mml-iit-updated-hiv

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -4,5 +4,5 @@ export const url =
     : "/api/v1/";
 export const token =
   process.env.NODE_ENV === "development"
-    ? "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzM0NTMxOTYyfQ.aNsbFLdm9WOTjbPfyAbZm7lA-TTg9G8M9PmaSvg5VVpFShKRekiRvr4HfSSxumREU3Fhm5IZXQ6bCfJNAks-fQ"
+    ? "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzM0NjI3Njc3fQ.b5nw-oDPxmenGzQzuVIHbasXfqLto03WAocTxHkEkl-n4qG2jQiI91DqLgURPK4YsPp28s83bolLOC1Pzna4nQ"
     : new URLSearchParams(window.location.search).get("jwt");
